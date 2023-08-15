3e8990fdd2ca1b8bc5dc137ffadc553c5a74d782
Nimesh9172 <nimeshdj123@gmail.com>
2023-08-15 20:33:41 +0530

    authentication finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,19 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import Authentication,{action as AuthAction} from './pages/Authentication'
+import { action as LogoutAction } from './pages/Logout';
+import { tokenLoader,checkAuthLoader } from './utils/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id:'root',
+    loader:tokenLoader,
     children: [
-      { index: true, element: <HomePage /> },
+      { index: true, element: <HomePage />,loader:checkAuthLoader },
       {
         path: 'events',
         element: <EventsRootLayout />,
@@ -44,6 +49,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader:checkAuthLoader
               },
             ],
           },
@@ -51,6 +57,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader:checkAuthLoader
           },
         ],
       },
@@ -59,6 +66,17 @@ const router = createBrowserRouter([
         element: <NewsletterPage />,
         action: newsletterAction,
       },
+      {
+        path: 'auth',
+        element: <Authentication/>,
+        action:AuthAction
+      },
+      {
+        path:'logout',
+        action:LogoutAction,
+        loader:checkAuthLoader
+
+      }
     ],
   },
 ]);
